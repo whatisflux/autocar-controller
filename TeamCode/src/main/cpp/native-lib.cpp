0fe4db0718ce54c272f62d7df888dcd8592bd483
Peter <PeterTheEarthling@gmail.com>
2019-05-28 15:33:06 -0400

    push from surface

--- TeamCode/src/main/cpp/native-lib.cpp
+++ TeamCode/src/main/cpp/native-lib.cpp
@@ -30,6 +30,9 @@ Java_org_firstinspires_ftc_robotcontroller_Vision_Vision_readBallPattern(JNIEnv 
 
 
 
+
+
+
     __android_log_print(ANDROID_LOG_VERBOSE, "whatIsFlux", "ERROR_LOG: debug 1: %lf debug2: %lf debug3: %lf",
         (double) debug1, (double) debug2, (double) debug3);
 
